8cc5d385120a9c64ed86d412730a365a581c86cc
Kunjan Shah <kunjanone@gmail.com>
2026-02-04 21:04:18 +0100

    Fix api suffix issue

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 //const API_URL = 'https://llm-prompts-backend.onrender.com/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://llm-prompts-backend.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://llm-prompts-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
